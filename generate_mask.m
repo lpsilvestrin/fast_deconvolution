74d4bfbf295d4956ed469ecf4ab163ac805e89ee
Luis <lpsilvestrin@inf.ufrgs.br>
2014-11-23 15:28:58 -0200

    generate_mask com algum erro de cálculo
    acredito que eu fiz o cálculo exatamente como o artigo explica. talvez
    tenha algum erro no logarítmo, mas eu já tentei usando o natural, base
    10 e 2 e o resultado continua errado.

---
+++ generate_mask.m
@@ -0,0 +1,22 @@
+function mask = generate_mask(unpadded_rows, unpadded_cols, padded_rows, padded_cols, alpha)
+mask = zeros(padded_rows, padded_cols);
+R = unpadded_rows;
+C = unpadded_cols;
+rc = padded_rows/2;
+cc = padded_cols/2;
+log_alpha = log((1-alpha)/alpha);
+nr = ceil(0.5*log_alpha/log(rc/(R/2)));
+nc = ceil(0.5*log_alpha/log(cc/(C/2)));
+for i=1:padded_rows
+    for j=1:padded_cols
+        p1 = i-rc;
+        p2 = R/2;
+        p3 = p1/p2;
+        p4 = 1 + p3*(2*nr);
+        p5 = j-cc;
+        p6 = C/2;
+        p7 = p5/p6;
+        p8 = 1 + p7*(2*nc);
+        mask(i,j) = 1/(p4*p8);
+    end
+end
