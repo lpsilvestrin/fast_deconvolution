c113e5593fef9a210e5d330edc5028cca5facc29
Luis <lpsilvestrin@inf.ufrgs.br>
2014-11-23 15:31:50 -0200

    comentario

--- fast_deconvolution.m
+++ fast_deconvolution.m
@@ -2,6 +2,6 @@ function img = fast_deconvolution(image, kernel, weights)
 
 
 function padded = padding(img, n)
-% Add n lines and cols under, above and in the sidesof the image. 
+% Add n lines and cols under, above and in the sides of the image. 
 % n = 2*max(kernel_cols, kernel_rows)
 padded = padarray(img, [n, n], 'replicate');
