1346c9edc791ff395af868f8104cedcd34731d57
Luis <lpsilvestrin@inf.ufrgs.br>
2014-11-23 22:02:09 -0200

    nada

--- fast_deconvolution_1d.m
+++ fast_deconvolution_1d.m
@@ -2,7 +2,7 @@ function deconvolved = fast_deconvolution_1d(img, kernel, weights)
 % deconvolve 1d image
 % step 1
 % finding derivatives
-sobelx = [[-1, 0, 1], [-2 0 2], [-1, 0, 1]];
+sobelx = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
 sobely = [[1 2 1] [0 0 0] [-1 -2 -1]];
 dx = conv2(double(img), double(sobelx));
 dxx = conv2(dx, sobelx);
