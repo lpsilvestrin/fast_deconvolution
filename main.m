f3582962f18f2c71c98e33d5cc17c97282d2c068
rgherdt <rgherdt@inf.ufrgs.br>
2014-11-29 12:01:00 -0200

    fillDerivs

--- main.m
+++ main.m
@@ -4,17 +4,21 @@ f = imread('cameraman.jpg');
 
 [rows, cols, chans] = size(f);
 
+
+% Generate matrix containing all needed derivatives from the image
+function derivs = fillDerivs(dx, dy)
+    [rows, cols] = size(dx); %assuming same size for both dx and dy
+    derivs = zeros(rows, cols, 5);
+    derivs(:, :, 1) = fft2(double(dx), rows, cols);            % dx
+    derivs(:, :, 2) = fft2(double(dy), rows, cols);            % dy
+    derivs(:, :, 3) = fft2(double(conv2(dx, dx)), rows, cols); % dxx
+    derivs(:, :, 4) = fft2(double(conv2(dy, dy)), rows, cols); % dyy
+    derivs(:, :, 5) = fft2(double(conv2(dx, dy)), rows, cols); % dxy
+
 % derivative of image
 sobelV = fspecial('sobel');
 sobelH = transpose(sobelV);
-
-% Matrix containing all needed derivatives from the image
-derivs = zeros(rows, cols, 5);
-derivs(:, :, 1) = fft2(double(sobelH), rows, cols);                % dx
-derivs(:, :, 2) = fft2(double(sobelV), rows, cols);                % dy
-derivs(:, :, 3) = fft2(double(conv2(sobelH, sobelH)), rows, cols); % dxx
-derivs(:, :, 4) = fft2(double(conv2(sobelV, sobelV)), rows, cols); % dyy
-derivs(:, :, 5) = fft2(double(conv2(sobelH, sobelV)), rows, cols); % dxy
+derivs(sobelH, sobelV);
 
 % Degrading function and its transforms
 h = kernel1;
