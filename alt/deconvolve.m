05f050c71abeb1afd5fb11e2c3056d8327027ccc
rgherdt <rgherdt@inf.ufrgs.br>
2014-11-29 18:20:07 -0200

    cleaning main up

--- alt/deconvolve.m
+++ alt/deconvolve.m
@@ -1,5 +1,14 @@
-function result = deconvolve(G, H, weights, w_set, derivs)
+function result = deconvolve(G, H, weights, w_set)
     [rows, cols, chans] = size(G);
+
+    % derivative set
+    sobelV = fspecial('sobel');
+    sobelH = transpose(sobelV);
+    dx = sobelH;
+    DX = fft2(dx, rows, cols);
+    dy = sobelV;
+    DY = fft2(dy, rows, cols);
+    derivs = fillDerivs(DX, DX, rows, cols);
 
     A = conj(H) .* H;
     for i = 1 : 5
@@ -9,5 +18,9 @@ function result = deconvolve(G, H, weights, w_set, derivs)
 
     W = fft2(w_set);
     B = conj(H) .* G;
+    for i = 1 : 5
+        S = weights(i) * (conj(derivs(:, :, i)) .* W(:, :, i));
+        B += S;
+    end;
 
     result = ifft2(B ./ A);
--- alt/main.m
+++ alt/main.m
@@ -15,29 +15,13 @@ G = fft2(f) .* H;
 % lambda array in equation
 weights1 = 0.001 * ones(1, 5);
 
-% derivative set
-sobelV = fspecial('sobel');
-sobelH = transpose(sobelV);
-dx = sobelH;
-DX = fft2(dx, rows, cols);
-dy = sobelV;
-DY = fft2(dy, rows, cols);
-derivs = fillDerivs(DX, DX, rows, cols);
-
-f0 = deconvolve(G, H, weights1, zeros(rows, cols, 5), derivs);
+f0 = deconvolve(G, H, weights1, zeros(rows, cols, 5));
 
 %% STEP 2
 
 sigma_s = 60;
 sigma_r = 0.4;
-f1 = NC(f0, sigma_s, sigma_r);
-
-%{
-for i = 1 : 5
-    S = weights1(i) * (conj(derivs(:, :, i)) .* W(:, :, i));
-    B += S;
-end;
-%}
+%f1 = NC(f0, sigma_s, sigma_r);
 
 %% STEP 3
 %% compute regularization priors
