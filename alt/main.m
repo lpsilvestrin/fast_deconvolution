a5cfa05242efeb7477b30c288ad1e8d49da12f83
rgherdt <rgherdt@inf.ufrgs.br>
2014-11-29 20:32:39 -0200

    readequando (codigo quebrado)

--- alt/main.m
+++ alt/main.m
@@ -1,27 +1,36 @@
 load 'kernels.mat';
 
-f = imread('cameraman.jpg');
+I = imread('cameraman.jpg');
+k = kernel1;
+J = conv2(I, k);
 
-[rows, cols, chans] = size(f);
+[rows, cols, chans] = size(I);
+h = sparse(rows*cols, rows*cols);
+
+f = reshape(transpose(I), 1, rows * cols);
+g = reshape(transpose(J)), 1, rows * cols);
 
 % Degrading function and its transforms
-h = kernel1;
-H = fft2(h, rows, cols);
 
 % Degraded image
 
+%g = conv2(double(f), h);
 G = fft2(f) .* H;
+g = ifft2(G);
 
 % lambda array in equation
 weights1 = 0.001 * ones(1, 5);
 
-f0 = deconvolve(G, H, weights1, zeros(rows, cols, 5));
+f0 = deconvolve(g, H, weights1, zeros(rows, cols, 5));
 
 %% STEP 2
 
 sigma_s = 60;
 sigma_r = 0.4;
-%f1 = NC(f0, sigma_s, sigma_r);
+f1 = NC(f0, sigma_s, sigma_r);
 
 %% STEP 3
 %% compute regularization priors
+weights2 = 0.05 * ones(1, 5);
+ws = compute_priors(f0);
+f2 = deconvolve(f1, H, weights2, ws);
