a5761503b31a2bf038df35e04abfb98452e14207
lpsilvestrin <lpsilvestrin@inf.ufrgs.br>
2014-11-30 17:47:43 -0200

    merge commit

--- alt/deconvolve.m
+++ alt/deconvolve.m
@@ -14,15 +14,23 @@ function result = deconvolve(g, H, weights, w_set)
     derivs(:, :, 4) = DY .* DY; % dyy
     derivs(:, :, 5) = DX .* DY; % dxy
 
-
+	S = 0;
     A = conj(H) .* H;
     for i = 1 : 5
-        S = weights(i) * (conj(derivs(:, :, i)) .* derivs(:, :, i));
-        A += S;
+        S += weights(i) * (conj(derivs(:, :, i)) .* derivs(:, :, i));
+    end;
+    for i = 1:size(A)(1)
+	A(1,:) += S;
     end;
 
     W = fft2(w_set);
-    B = conj(H) .* G;
+    B = conj(H);
+	for i = 1:size(B)(1)
+		B(1,:) = B(1,:) .* G;
+	end;
+	
+	derivs = reshape(derivs, [size(W, 1) size(W,2)]);
+	derivs = transpose(derivs);
     for i = 1 : 5
         S = weights(i) * (conj(derivs(:, :, i)) .* W(:, :, i));
         B += S;
--- alt/main.m
+++ alt/main.m
@@ -1,7 +1,7 @@
 load 'kernels.mat'; 
-I = imread('cameraman.jpg');
-[rows, cols, chans] = size(I);
 
+I = imread('tst.JPG');
+[rows, cols, chans] = size(I);
 k = kernel1;
 J = conv2(I, k)(3 : rows + 2, 3 : cols + 2);
 
@@ -10,6 +10,7 @@ h = sparse(rows*cols, rows*cols);
 
 f = reshape(transpose(I), 1, rows * cols);
 g = reshape(transpose(J), 1, rows * cols);
+
 h = sparse(rows * cols, rows * cols);
 for i = 1 : (rows - k_dim(1))
     for j = 1 : (cols - k_dim(2))
@@ -24,7 +25,7 @@ end
 % Degrading function and its transforms
 
 % Degraded image
-
+H = fft2(h);
 %g = conv2(double(f), h);
 %G = fft2(f) .* H;
 %g = ifft2(G);
@@ -32,16 +33,15 @@ end
 % lambda array in equation
 weights1 = 0.001 * ones(1, 5);
 
-%f0 = deconvolve(g, H, weights1, zeros(rows, cols, 5));
+f0 = deconvolve(g, H, weights1, zeros(rows, cols, 5));
 
 %% STEP 2
-
 sigma_s = 60;
 sigma_r = 0.4;
-%f1 = NC(f0, sigma_s, sigma_r);
+f1 = NC(f0, sigma_s, sigma_r);
 
 %% STEP 3
 %% compute regularization priors
 weights2 = 0.05 * ones(1, 5);
-%ws = compute_priors(f0);
-%f2 = deconvolve(f1, H, weights2, ws);
+ws = compute_priors(f1);
+f2 = deconvolve(g, H, weights2, ws);
