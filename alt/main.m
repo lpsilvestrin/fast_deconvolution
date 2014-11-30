40af30351504d01be854982ecb84b65a8656690d
rgherdt <rgherdt@inf.ufrgs.br>
2014-11-30 18:03:49 -0200

    adicionei ruído; etapa 2

--- alt/main.m
+++ alt/main.m
@@ -7,21 +7,23 @@ f = imread('cameraman.jpg');
 % Degrading function and its transforms
 h = kernel1;
 H = fft2(h, rows, cols);
+f = imnoise(f, 'gaussian', 0.1, 100);
 
 % Degraded image
 
 G = fft2(f) .* H;
+g = ifft2(G);
 
 % lambda array in equation
 weights1 = 0.001 * ones(1, 5);
 
-f0 = deconvolve(G, H, weights1, zeros(rows, cols, 5));
+f0 = deconvolve(g, H, weights1, zeros(rows, cols, 5));
 
 %% STEP 2
 
 sigma_s = 60;
 sigma_r = 0.4;
-%f1 = NC(f0, sigma_s, sigma_r);
+f1 = RF(f0, sigma_s, sigma_r);
 
 %% STEP 3
 %% compute regularization priors
