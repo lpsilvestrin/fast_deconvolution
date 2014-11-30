b181ba1fc222952b2ecd2bc5309fe315cae75430
rgherdt <rgherdt@inf.ufrgs.br>
2014-11-30 17:27:07 -0200

    Revert "trying to create h"
    This reverts commit a45aadb4161d771d511a3e267a8faca8d4765020.

--- alt/main.m
+++ alt/main.m
@@ -1,47 +1,36 @@
-load 'kernels.mat'; 
+load 'kernels.mat';
+
 I = imread('cameraman.jpg');
-[rows, cols, chans] = size(I);
-
 k = kernel1;
-J = conv2(I, k)(3 : rows + 2, 3 : cols + 2);
+J = conv2(I, k);
 
-k_dim = size(k);
+[rows, cols, chans] = size(I);
 h = sparse(rows*cols, rows*cols);
 
 f = reshape(transpose(I), 1, rows * cols);
-g = reshape(transpose(J), 1, rows * cols);
-h = sparse(rows * cols, rows * cols);
-for i = 1 : (rows - k_dim(1))
-    for j = 1 : (cols - k_dim(2))
-%        printf('expanding col %d, row %d\n', j, i);
-%        printf('printing %d, %d\n', i, j);
-        h(i * j, :) = expand_kernel(k, rows, cols, i, j);
-%        arr = reshape(transpose(arr), 1, rows * cols);
-%        h(i * j, :) = arr;
-    end
-end
+g = reshape(transpose(J)), 1, rows * cols);
 
 % Degrading function and its transforms
 
 % Degraded image
 
 %g = conv2(double(f), h);
-%G = fft2(f) .* H;
-%g = ifft2(G);
+G = fft2(f) .* H;
+g = ifft2(G);
 
 % lambda array in equation
 weights1 = 0.001 * ones(1, 5);
 
-%f0 = deconvolve(g, H, weights1, zeros(rows, cols, 5));
+f0 = deconvolve(g, H, weights1, zeros(rows, cols, 5));
 
 %% STEP 2
 
 sigma_s = 60;
 sigma_r = 0.4;
-%f1 = NC(f0, sigma_s, sigma_r);
+f1 = NC(f0, sigma_s, sigma_r);
 
 %% STEP 3
 %% compute regularization priors
 weights2 = 0.05 * ones(1, 5);
-%ws = compute_priors(f0);
-%f2 = deconvolve(f1, H, weights2, ws);
+ws = compute_priors(f0);
+f2 = deconvolve(f1, H, weights2, ws);
