37bb5fd04d479dfce71c78ac6155ec5f9b964f56
rgherdt <rgherdt@inf.ufrgs.br>
2014-12-01 10:10:31 -0200

    compute_priors

--- alt/compute_priors.m
+++ alt/compute_priors.m
@@ -1,6 +1,35 @@
 function ws = compute_priors(f)
     [rows, cols] = size(f);
 
+    
+    dx   = [-0.5, 0, 0.5];
+    dy   = [-0.5; 0; 0.5];
+    dxx  = [-1 / 1.4142, 2 / 1.4142, -1 / 1.4142];
+    dyy  = [-1 / 1.4142; 2 / 1.4142; -1 / 1.4142]; 
+    dxy  = [-1.4142, 1.4142, 0; 1.4142, -1.4142, 0 ; 0, 0, 0];
+
+    dx  = rot90(dx,2 ); %rot 180 deg, same as flipud(fliplr(dx));    
+    dy  = rot90(dy,2 );
+    dxx = rot90(dxx,2);
+    dyy = rot90(dyy,2);
+    dxy = rot90(dxy,2); 
+    
+    conj_dx  = rot90(dx,2 ); %rot 180 deg, same as flipud(fliplr(dx));    
+    conj_dy  = rot90(dy,2 );
+    conj_dxx = rot90(dxx,2);
+    conj_dyy = rot90(dyy,2);
+    conj_dxy = rot90(dxy,2);    
+
+    threshold1 = 0.065;
+    threshold2 = 0.0325;
+    ws(:, :, 1) = calc_ratio(f, dx, threshold1);
+    ws(:, :, 2) = calc_ratio(f, dy, threshold1);
+    ws(:, :, 3) = calc_ratio(f, dxx, threshold2);
+    ws(:, :, 4) = calc_ratio(f, dyy, threshold2);
+    ws(:, :, 5) = calc_ratio(f, dxy, threshold2);
+    
+
+    %{
     dy = fspecial('sobel');
     dx = transpose(dy);
     DX = fft2(dx);
@@ -17,17 +46,14 @@ function ws = compute_priors(f)
 
     threshold = 0.065;
     for i = 1 : 2
-        der = conv2(double(f), fst_derivs(:, :, i), 'same');
-        denom = (threshold ./ der) .^ 4 + 1;
-        ws(:, :, i) = der ./ denom;
+        ws(:, :, i) = calc_ratio(f, fst_derivs(:, :, i), 0.065);
     end
 
     threshold = 0.0325;
     for i = 1 : 3
-        der = conv2(double(f), snd_derivs(:, :, i), 'same');
-        denom = (threshold ./ der) .^ 4 + 1;
-        ws(:, :, i + 2) = der ./ denom;
+        ws(:, :, i + 2) = calc_ratio(f, snd_derivs(:, :, i), 0.0325);
     end
+    %}
     %{
     ws = zeros(rows, cols);
     derivs = getA1(rows, cols);
@@ -40,5 +66,7 @@ function ws = compute_priors(f)
     ws(:, :) = numer ./ denom;
     %}
 
-
-
+function ratio = calc_ratio(f, der, threshold)
+    der = conv2(double(f), der, 'same');
+    denom = (threshold ./ der) .^ 4 + 1;
+    ratio = der ./ denom;
