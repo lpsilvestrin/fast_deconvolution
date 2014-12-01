3084189760f0b710f3de86f00de95218b6fcbdd8
rgherdt <rgherdt@inf.ufrgs.br>
2014-12-01 10:48:58 -0200

    arrumando o f1

--- alt/main.m
+++ alt/main.m
@@ -22,11 +22,11 @@ f0 = deconvolve(gn, H, weights1, fft2(zeros(rows, cols, 5)));
 %% STEP 2
 
 sigma_s = 60;
-sigma_r = 0.03;
+sigma_r = 0.3;
 f1 = RF(im2double(uint8(real(f0))), sigma_s, sigma_r);
 f1 = im2uint8(f1);
 
 %% STEP 3
 %% compute regularization priors
 ws = compute_priors(f1);
-f2 = deconvolve(f1, H, 0.05 * ones(1, 5), uint8(ws));
+f2 = deconvolve(f1, H, 0.1 * ones(1, 5), real(ws));
