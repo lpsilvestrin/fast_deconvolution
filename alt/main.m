7d091c395a0dc60cb2277d97718cfae07e653524
rgherdt <rgherdt@inf.ufrgs.br>
2014-12-02 01:07:00 -0200

    main.m

--- alt/main.m
+++ alt/main.m
@@ -1,21 +1,22 @@
 load 'kernels.mat';
 
-f = imread('cameraman.bmp');
+f = imread('cameraman.jpg');
+[rows, cols, chans] = size(f);
+
 h = kernel1;
 pad_amount = 2 * length(h);
 
+f = padding(f, pad_amount);
+[rows, cols, chans] = size(f);
 % generate gn degraded without pad 
 g = zeros(size(f));
 for c = 1 : chans
     g(:, :, c) = conv2(double(f(:, :, c)), h, 'same');
 end;
-gn = imnoise(g, 'gaussian', 0.04, 10);
+%gn = imnoise(g, 'gaussian', 0.04, 10);
+gn = g + randn(rows, cols, chans) * 0.01;
 %%%%%%%%% gn generated %%%%%%%%
 
-% Padding gn
-
-gn = padding(gn, pad_amount);
-[rows, cols, chans] = size(gn);
 
 % Degrading function and its transforms
 h_big = getBigKernel(rows, cols, h);
@@ -24,9 +25,18 @@ H = fft2(h_big);
 % Degraded image
 
 % lambda array in equation
-weights1 = 0.001 * ones(1, 5);
+weight1 = 0.001;
 
-f0 = deconvolve(gn, H, weights1, fft2(zeros(rows, cols, 5)));
+A0 = real(conj(H) .* H);
+A1 = getA1(rows, cols);
+A10 = A0 + weight1 .* A1;
+f0 = zeros(rows, cols, chans);
+for c = 1 : chans
+    B0(:,:,c) = conj(H) .* fft2(gn(:,:,c));
+    f0(:,:,c) = real(ifft2(B0(:,:,c) ./ A10));
+endfor;
+
+%f0 = deconvolve(gn, H, weights1, fft2(zeros(rows, cols, 5)));
 
 %% STEP 2
 
@@ -38,7 +48,15 @@ f1 = im2uint8(f1);
 %% STEP 3
 %% compute regularization priors
 %ws = compute_priors(f1);
-f2 = deconvolve(f1, H, 0.1 * ones(1, 5), fft2(zeros(rows, cols, 5)));
+%f2 = deconvolve(f1, H, 0.1 * ones(1, 5), fft2(zeros(rows, cols, 5)));
+weight2 = 0.05;
+A12  = A0 + weight2 .* A1;
+f2 = zeros(rows, cols, chans);
+for c = 1 : chans 
+    b1 = get_b1(f1(:, :, c));
+    B = B0(:, :, c) + weight2 .* fft2(b1);    
+    f2(:,:,c)  = real(ifft2(B ./ A12)); 
+end;
 
 %% Remove padding
 f3 = f2(pad_amount : rows - pad_amount,
